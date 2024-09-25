1aafb0556a75ba059da36faba1fdb17f2c2257ec
dreamerme <dreamerme@users.noreply.github.com>
2024-09-24 20:18:56 -0600

    week-2

---
+++ app/week-2/student-info.js
@@ -0,0 +1,9 @@
+import Link from "next/link";
+export default function StudentInfo() {
+    return (
+        <main>
+            <p>Yuan Lyu</p>
+            <Link href="https://github.com/dreamerme/cprg306-assignments">https://github.com/dreamerme/cprg306-assignments</Link>
+        </main>
+    );
+}
