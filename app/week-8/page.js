c630ebcb36db6e47b73ec8764b49878663672b67
dreamerme <dreamerme@users.noreply.github.com>
2024-11-05 21:03:28 -0700

    week-8

---
+++ app/week-8/page.js
@@ -0,0 +1,40 @@
+"use client"; 
+import React, { useState } from "react";
+import NewItem from "./new-item.js";
+import ItemList from "./item-list.js";
+import itemsData from "./items.json"; 
+import MealIdeas from "./meal-ideas.js";
+
+export default function Page() {
+    const [items, setItems] = useState(itemsData);
+    const [selectedItemName, setSelectedItemName] = useState("");
+
+    const handleAddItem = (newItem) => {
+        setItems([...items, newItem]);
+    };
+
+    const handleItemSelect = (itemName) => {
+        console.log("Selected item:", itemName.name);
+        const cleanedItemName = itemName.name
+            .split(",")[0]  // Remove anything after a comma
+            .replace(/[^\w\s]/gu, "")  // Remove emojis and special characters
+            .trim();
+        console.log("Cleaned item name:", cleanedItemName);
+        setSelectedItemName(cleanedItemName);
+    };
+
+    return (
+        <main className="p-6 min-h-screen bg-gray-800">
+            <h1 className="text-4xl font-bold text-white mb-6">Shopping List</h1>
+            <section className="flex flex-row items-start">
+                <div className="w-1/2">
+                    <NewItem onAddItem={handleAddItem} />
+                    <ItemList items={items} onItemSelect={handleItemSelect} />
+                </div>
+                <div className="w-1/2">
+                    <MealIdeas ingredient={selectedItemName} /> 
+                </div>
+            </section>
+        </main>
+    );
+}
